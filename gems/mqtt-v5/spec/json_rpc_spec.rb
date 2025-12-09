8183997d5baf6bf8f3b16781f5b0d6e9c9264e7d
Grant Gardner <grant@lastweekend.com.au>
2025-12-09 15:20:32 +0100

    Update json_rpc_spec to follow request_response pattern
    - Rewritten to use with_client pattern instead of with_client_pair
    - All tests skipped as placeholders for future JSON-RPC implementation
    - Tests define expected API for json_rpc_requester/responder

--- gems/mqtt-v5/spec/json_rpc_spec.rb
+++ gems/mqtt-v5/spec/json_rpc_spec.rb
@@ -9,49 +9,71 @@ module MQTT
         base.class_eval do
           describe 'JSON-RPC' do
             it 'basic JSON-RPC call' do
-              with_client_pair do |requester_client, responder_client|
+              skip 'JSON-RPC implementation - to be implemented'
+              
+              with_client do |responder_client|
+                responder_client.connect
                 responder_client.json_rpc_responder('test/api') do |method, params|
                   { method: method, params: params }
                 end
                 
-                rpc = requester_client.json_rpc_requester('test/api')
-                result = rpc.call('test_method', foo: 'bar', timeout: 2)
-                
-                _(result[:method]).must_equal('test_method')
-                _(result[:params][:foo]).must_equal('bar')
+                with_client(session_store: responder_client.class.memory_store) do |requester_client|
+                  requester_client.connect
+                  rpc = requester_client.json_rpc_requester('test/api')
+                  result = rpc.call('test_method', foo: 'bar', timeout: 2)
+                  
+                  _(result[:method]).must_equal('test_method')
+                  _(result[:params][:foo]).must_equal('bar')
+                end
               end
             end
 
             it 'method_missing for natural Ruby calls' do
-              with_client_pair do |requester_client, responder_client|
+              skip 'JSON-RPC implementation - to be implemented'
+              
+              with_client do |responder_client|
+                responder_client.connect
                 responder_client.json_rpc_responder('test/api') do |method, params|
                   { success: true, called: method }
                 end
                 
-                rpc = requester_client.json_rpc_requester('test/api')
-                result = rpc.restart(device_id: '123')
-                
-                _(result[:success]).must_equal(true)
-                _(result[:called]).must_equal('restart')
+                with_client(session_store: responder_client.class.memory_store) do |requester_client|
+                  requester_client.connect
+                  rpc = requester_client.json_rpc_requester('test/api')
+                  result = rpc.restart(device_id: '123')
+                  
+                  _(result[:success]).must_equal(true)
+                  _(result[:called]).must_equal('restart')
+                end
               end
             end
 
             it 'JSON-RPC error handling' do
-              with_client_pair do |requester_client, responder_client|
+              skip 'JSON-RPC implementation - to be implemented'
+              
+              with_client do |responder_client|
+                responder_client.connect
                 responder_client.json_rpc_responder('test/api') do |method, params|
                   raise 'Something went wrong'
                 end
                 
-                rpc = requester_client.json_rpc_requester('test/api')
-                
-                err = _(proc { rpc.call('failing_method', timeout: 2) }).must_raise(JsonRpcError)
-                _(err.code).must_equal(-32603)
-                _(err.message).must_equal('Something went wrong')
+                with_client(session_store: responder_client.class.memory_store) do |requester_client|
+                  requester_client.connect
+                  rpc = requester_client.json_rpc_requester('test/api')
+                  
+                  err = _(proc { rpc.call('failing_method', timeout: 2) }).must_raise(JsonRpcError)
+                  _(err.code).must_equal(-32603)
+                  _(err.message).must_equal('Something went wrong')
+                end
               end
             end
 
             it 'object dispatch' do
-              with_client_pair do |requester_client, responder_client|
+              skip 'JSON-RPC implementation - to be implemented'
+              
+              with_client do |responder_client|
+                responder_client.connect
+                
                 service = Object.new
                 def service.restart(device_id:)
                   { restarted: device_id }
@@ -59,10 +81,13 @@ module MQTT
                 
                 responder_client.json_rpc_responder('test/api', service)
                 
-                rpc = requester_client.json_rpc_requester('test/api')
-                result = rpc.restart(device_id: 'device-1')
-                
-                _(result[:restarted]).must_equal('device-1')
+                with_client(session_store: responder_client.class.memory_store) do |requester_client|
+                  requester_client.connect
+                  rpc = requester_client.json_rpc_requester('test/api')
+                  result = rpc.restart(device_id: 'device-1')
+                  
+                  _(result[:restarted]).must_equal('device-1')
+                end
               end
             end
           end
